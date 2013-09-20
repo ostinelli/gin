3a2698f4e4c33ab055afa6ed1d57b95ef7af8f16
Roberto Ostinelli <roberto@ostinelli.net>
2013-09-19 22:55:32 -0400

    Add router multiple params test

--- spec/core/router_spec.lua
+++ spec/core/router_spec.lua
@@ -20,11 +20,12 @@ describe("Router", function()
             routes.GET("/users", { controller = "users", action = "index" })
             routes.GET("/users/:id", { controller = "users", action = "show" })
             routes.PUT("/users/:id", { controller = "users", action = "edit" })
+            routes.DELETE("/users/:user_id/messages/:id", { controller = "messages", action = "destroy" })
 
             router.dispatchers = routes.dispatchers
         end)
 
-        it("returns the controller, action and params", function()
+        it("returns the controller, action and params for a single param", function()
             ngx = {
                 var = {
                     uri = "/users/roberto",
@@ -37,6 +38,21 @@ describe("Router", function()
             assert.are.same("users_controller", controller)
             assert.are.same("show", action)
             assert.are.same({ id = "roberto" }, params)
+        end)
+
+        it("returns the controller, action and params for a multiple params", function()
+            ngx = {
+                var = {
+                    uri = "/users/roberto/messages/123",
+                    request_method = "DELETE"
+                }
+            }
+
+            controller, action, params = router.match(ngx)
+
+            assert.are.same("messages_controller", controller)
+            assert.are.same("destroy", action)
+            assert.are.same({ user_id = "roberto", id = "123" }, params)
         end)
     end)
 end)
