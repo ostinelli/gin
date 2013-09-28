702192a3d03eb49bbdb6e6f8fe9884adddb02dbe
Roberto Ostinelli <roberto@ostinelli.net>
2013-09-28 19:22:29 -0400

    Add POST route to pages controller

--- config/routes.lua
+++ config/routes.lua
@@ -5,6 +5,7 @@ routes.GET("/users", { controller = "users", action = "index" })
 routes.GET("/users/:id", { controller = "users", action = "show" })
 routes.GET("/users/:user_id/messages", { controller = "messages", action = "index" })
 routes.GET("/users/:user_id/messages/:id", { controller = "messages", action = "show" })
+routes.POST("/pages/body_and_url", { controller = "pages", action = "body_and_url" })
 routes.GET("/pages/(.+)", { controller = "pages", action = "catch_all" })
 
 return routes
