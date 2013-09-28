22b594a8c3f39adecf31f7bf5dbdc28893ed0549
Roberto Ostinelli <roberto@ostinelli.net>
2013-09-28 19:21:59 -0400

    Restyle message controller

--- app/controllers/messages_controller.lua
+++ app/controllers/messages_controller.lua
@@ -5,7 +5,11 @@ function MessagesController:index()
 end
 
 function MessagesController:show()
-    return { message = { id = self.params.id, recipient = self.params.user_id, body = "Ralis is awesome." } }
+    return { message = {
+        id = self.params.id,
+        recipient = self.params.user_id,
+        body = "Ralis is awesome."
+    } }
 end
 
 return MessagesController
