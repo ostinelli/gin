c4fb3c6bab088c81e867b706cdf30205ea7b9d5a
Roberto Ostinelli <roberto@ostinelli.net>
2013-09-28 16:24:17 -0400

    Add getters for ngx params in request

--- core/request.lua
+++ core/request.lua
@@ -1,18 +1,35 @@
 Request = {}
 Request.__index = Request
 
-function Request.new(options)
-    options = options or {}
-
+function Request.new(ngx)
     local instance = {
-        method = options.method or 'GET',
-        url = options.url or "/",
-        query = options.query or {},
-        headers = options.headers or {},
-        body = options.body or "",
+        ngx = ngx,
+        body = ngx.req.read_body(),
+        __cache = {}
     }
     setmetatable(instance, Request)
     return instance
+end
+
+function Request:__index(index)
+    local out = rawget(rawget(self, '__cache'), index)
+    if out then return out end
+
+    if index == 'uri_params' then
+        self.__cache[index] = ngx.req.get_uri_args()
+        return self.__cache[index]
+
+    elseif index == 'headers' then
+        self.__cache[index] = ngx.req.get_headers()
+        return self.__cache[index]
+
+    elseif index == 'post_params' then
+        self.__cache[index] = ngx.req.get_post_args()
+        return self.__cache[index]
+
+    else
+        return rawget(self, index)
+    end
 end
 
 return Request
--- spec/core/request_spec.lua
+++ spec/core/request_spec.lua
@@ -1,35 +1,68 @@
 require 'spec/spec_helper'
 
 describe("Request", function()
-    describe(".new", function()
-        describe("when no options are passed in", function()
-            it("initializes an instance with defaults", function()
-                local request = Request.new()
+    before_each(function()
+        ngx = {
+            req = {
+                read_body = function() return "" end,
+                get_uri_args = function() return { uri_param = '2' } end,
+                get_headers = function() return { ["Content-Type"] = "application/json" } end,
+                get_post_args = function() return { body_param = '2' } end
+            }
+        }
 
-                assert.are.same('GET', request.method)
-                assert.are.same("/", request.url)
-                assert.are.same({}, request.query)
-                assert.are.same({}, request.headers)
-                assert.are.same("", request.body)
-            end)
-        end)
+            -- var = {
+                -- content_length = "1000",
+                -- content_type="text/html",
+                -- filename="index.html",
+                -- host="www.example.com",
+                -- hostname="example",
+                -- query_string="a=b&1=2",
+                -- request_method="GET",
+                -- schema="http",
+                -- uri="/uri",
+                -- http_user_agent="Chrome",
+                -- http_referer="http://another.com",
+                -- server_port=80
+            -- }
+            -- req = {
+            --     get_headers=function() end,
+            --     get_post_args=function() end,
+            --     get_uri_args=function() end,
+            --     read_body=function() end
+            -- }
+        -- }
 
-        describe("when options are passed in", function()
-            it("saves them to the instance", function()
-                local request = Request.new({
-                    method = 'PUT',
-                    url = "/users",
-                    query = { page = 2 },
-                    headers = { ["X-Custom"] = "custom" },
-                    body = "The body."
-                })
+        request = Request.new(ngx)
+    end)
 
-                assert.are.same('PUT', request.method)
-                assert.are.same("/users", request.url)
-                assert.are.same({ page = 2 }, request.query)
-                assert.are.same({ ["X-Custom"] = "custom" }, request.headers)
-                assert.are.same("The body.", request.body)
-            end)
-        end)
+    after_each(function()
+        ngx = nil
+        request = nil
+    end)
+
+    it("reads the body on init", function()
+        spy.on(ngx.req, "read_body")
+        local new_request = Request.new(ngx)
+
+        assert.spy(ngx.req.read_body).was.called(1)
+
+        ngx.req.read_body:revert()
+    end)
+
+    it("returns nil for unset attrs", function()
+        assert.are.same(nil, request.unexisting_attr)
+    end)
+
+    it("returns uri_params", function()
+        assert.are.same({ uri_param = '2' }, request.uri_params)
+    end)
+
+    it("returns headers", function()
+        assert.are.same({ ["Content-Type"] = "application/json" }, request.headers)
+    end)
+
+    it("returns post_params", function()
+        assert.are.same({ body_param = '2' }, request.post_params)
     end)
 end)
