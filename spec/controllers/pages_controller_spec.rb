c801d58ee885a387ba2119e6107cc4ac89debeb4
Roberto Ostinelli <roberto@ostinelli.net>
2013-09-28 19:32:58 -0400

    Finally made the test runner work

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,11 +4,16 @@ describe("UsersController", function()
 
     describe("#index", function()
         it("responds with a list of users", function()
+            local body = CJSON.encode({ user = "roberto" })
             local response = hit({
                 method = 'POST',
                 url = "/pages/body_and_url",
                 uri_params = { page = 2 },
-                body = CJSON.encode({ user = "roberto" })
+                body = body,
+                headers = {
+                    ["content-type"] = "application/json",
+                    ["content-length"] = tostring(body:len()),
+                }
             })
 
             assert.are.same(200, response.status)
