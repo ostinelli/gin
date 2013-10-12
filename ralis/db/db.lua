16e66587e8aa2af700b32a75abd224552f8ee785
Roberto Ostinelli <roberto@ostinelli.net>
2013-10-12 19:12:12 -0400

    Add ralis.db.db tests.
    [#58680752]

--- ralis/db/db.lua
+++ ralis/db/db.lua
@@ -1,7 +1,26 @@
 local Database = {}
 Database.__index = Database
 
+local required_options = {
+    adapter = true,
+    host = true,
+    port = true,
+    database = true,
+    user = true,
+    password = true,
+    pool = true
+}
+
 function Database.new(options)
+    -- check for required params
+    local remaining_options = required_options
+    for k, _ in pairs(options) do remaining_options[k] = nil end
+    local missing_options = {}
+    for k, _ in pairs(remaining_options) do table.insert(missing_options, k) end
+
+    if #missing_options > 0 then error("missing required database options: " .. table.concat(missing_options, ', ')) end
+
+    -- init instance
     local adapter = require('ralis.db.adapters.' .. options.adapter)
     local orm = require('ralis.db.orm.' .. options.adapter)
 
---
+++ spec/db/db_spec.lua
@@ -0,0 +1,111 @@
+require 'spec.spec_helper'
+
+describe("Database", function()
+    before_each(function()
+        db = require 'ralis.db.db'
+        options = {
+            adapter = 'mysql',
+            host = "127.0.0.1",
+            port = 3306,
+            database = "ralis_development",
+            user = "root",
+            password = "",
+            pool = 5
+        }
+        package.loaded['ralis.db.adapters.mysql'] = {}
+        package.loaded['ralis.db.orm.mysql'] = {}
+    end)
+
+    after_each(function()
+        db = nil
+        options = nil
+        package.loaded['ralis.db.adapters.mysql'] = nil
+        package.loaded['ralis.db.orm.mysql'] = nil
+        package.loaded['ralis.db.db'] = nil
+    end)
+
+    describe(".new", function()
+        before_each(function()
+            package.loaded['ralis.db.adapters.mysql'] = { name = 'adapter' }
+            package.loaded['ralis.db.orm.mysql'] = { name = 'orm' }
+        end)
+
+        describe("when all the required options are passed", function()
+            it("initializes an instance", function()
+                local DB = db.new(options)
+
+                assert.are.equal(options, DB.options)
+                assert.are.equal('adapter', DB.adapter.name)
+                assert.are.equal('orm', DB.orm.name)
+            end)
+        end)
+
+        describe("when not all the required options are passed", function()
+            it("raises an error", function()
+                options = {
+                    adapter = 'mysql',
+                    host = "127.0.0.1",
+                    user = "root",
+                    password = "",
+                    pool = 5
+                }
+
+                ok, err = pcall(function() return db.new(options) end)
+                assert.are.equal(false, ok)
+                assert.are.not_equals(true, string.match(err, "missing required database options: database, port"))
+            end)
+        end)
+    end)
+
+    describe(".query", function()
+        before_each(function()
+            arg1, arg2 = nil, nil
+            package.loaded['ralis.db.adapters.mysql'] = {
+                query = function(...) arg1, arg2 = ... end
+            }
+            DB = db.new(options)
+        end)
+
+        after_each(function()
+            arg1 = nil
+            arg2 = nil
+            DB = nil
+        end)
+
+        it("calls the query on the adapter", function()
+            local sql = "SELECT 1"
+
+            DB:query(sql)
+
+            assert.are.same(options, arg1)
+            assert.are.same(sql, arg2)
+        end)
+
+    end)
+
+    describe(".define_model", function()
+        before_each(function()
+            arg1, arg2, arg3 = nil, nil, nil
+            package.loaded['ralis.db.orm.mysql'] = {
+                define_model = function(...) arg1, arg2, arg3 = ... end
+            }
+            DB = db.new(options)
+        end)
+
+        after_each(function()
+            arg1 = nil
+            arg2 = nil
+            arg3 = nil
+            DB = nil
+        end)
+
+        it("calls the query on the adapter", function()
+            DB:define_model('User', 'users')
+
+            assert.are.same(DB, arg1)
+            assert.are.same('User', arg2)
+            assert.are.same('users', arg3)
+        end)
+
+    end)
+end)
