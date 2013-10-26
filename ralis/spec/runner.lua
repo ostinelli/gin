485123940b428323d684b62fa9598325c503e340
Roberto Ostinelli <roberto@ostinelli.net>
2013-10-26 13:10:38 -0400

    Add model test helpers.
    [Finishes #59527220]

--- ralis/spec/runner.lua
+++ ralis/spec/runner.lua
@@ -1,5 +1,10 @@
 require 'ralis.spec.init'
 
+-- mock dbs
+package.loaded['ralis.db.adapters.mysql'] = {}
+require 'config.database'
+
+-- add integration runner
 local IntegrationRunner = require 'ralis.spec.runners.integration'
 
 -- helpers
