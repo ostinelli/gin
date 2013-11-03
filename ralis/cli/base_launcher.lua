ed80d1a25b6a1a61659d9c5f16c31424c6a5d767
Roberto Ostinelli <roberto@ostinelli.net>
2013-11-02 20:11:51 -0400

    Refactor base launcher.
    [#60027136]

--- ralis/cli/base_launcher.lua
+++ ralis/cli/base_launcher.lua
@@ -3,6 +3,36 @@ local lfs = require 'lfs'
 
 require 'ralis.core.ralis'
 
+
+local function create_dirs(necessary_dirs)
+    for _, dir in pairs(necessary_dirs) do
+        lfs.mkdir(dir)
+    end
+end
+
+local function create_nginx_conf(nginx_conf_file_path, nginx_conf_content)
+    local fw = io.open(nginx_conf_file_path, "w")
+    fw:write(nginx_conf_content)
+    fw:close()
+end
+
+local function remove_nginx_conf(nginx_conf_file_path)
+    os.remove(nginx_conf_file_path)
+end
+
+local function nginx_command(nginx_conf_file_path, nginx_signal)
+    return os.execute("nginx " .. nginx_signal .. " -p `pwd`/ -c " .. nginx_conf_file_path .. " 2>/dev/null")
+end
+
+local function start_nginx(nginx_conf_file_path)
+    return nginx_command(nginx_conf_file_path, '')
+end
+
+local function stop_nginx(nginx_conf_file_path)
+    return nginx_command(nginx_conf_file_path, '-s stop')
+end
+
+
 local BaseLauncher = {}
 BaseLauncher.__index = BaseLauncher
 
@@ -19,45 +49,18 @@ function BaseLauncher.new(nginx_conf_content, nginx_conf_file_path)
 end
 
 function BaseLauncher:start()
-    self:create_dirs()
-    self:create_nginx_conf()
+    create_dirs(self.necessary_dirs)
+    create_nginx_conf(self.nginx_conf_file_path, self.nginx_conf_content)
 
-    return self:start_nginx()
+    return start_nginx(self.nginx_conf_file_path)
 end
 
 function BaseLauncher:stop()
-    result = self:stop_nginx()
-    self:remove_nginx_conf()
+    result = stop_nginx(self.nginx_conf_file_path)
+    remove_nginx_conf(self.nginx_conf_file_path)
 
     return result
 end
 
-function BaseLauncher:start_nginx()
-    return self:nginx_command('')
-end
-
-function BaseLauncher:stop_nginx()
-    return self:nginx_command('-s stop')
-end
-
-function BaseLauncher:nginx_command(nginx_signal)
-    return os.execute("nginx " .. nginx_signal .. " -p `pwd`/ -c " .. self.nginx_conf_file_path .. " 2>/dev/null")
-end
-
-function BaseLauncher:create_dirs()
-    for _, dir in pairs(self.necessary_dirs) do
-        lfs.mkdir(dir)
-    end
-end
-
-function BaseLauncher:create_nginx_conf()
-    local fw = io.open(self.nginx_conf_file_path, "w")
-    fw:write(self.nginx_conf_content)
-    fw:close()
-end
-
-function BaseLauncher:remove_nginx_conf()
-    os.remove(self.nginx_conf_file_path)
-end
 
 return BaseLauncher
