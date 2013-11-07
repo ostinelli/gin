25f67157febfc5f29e7b2ec5dea72c2cb0cf74cb
Roberto Ostinelli <roberto@ostinelli.net>
2013-11-07 13:47:31 -0500

    Add header to console.

--- zebra/cli/console.lua
+++ zebra/cli/console.lua
@@ -1,9 +1,12 @@
+local ansicolors = require 'ansicolors'
+
 require 'zebra.core.zebra'
 
 
 local ZebraConsole = {}
 
 function ZebraConsole.start()
+    print(ansicolors("Loading %{cyan}" .. Zebra.env .. "%{reset} environment (Zebra v" .. Zebra.version .. ")"))
     os.execute("lua -i -e \"require 'zebra.core.zebra' require 'zebra.core.detached' require 'zebra.core.init'\"")
 end
 
