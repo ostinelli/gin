96fc22d5a906373828fa1f3e11f6b535ce9c28f7
Roberto Ostinelli <roberto@ostinelli.net>
2013-11-11 09:02:24 -0500

    Remove unnecessary modules.

--- zebra/core/init.lua
+++
@@ -1,8 +0,0 @@
--- -- init module dependencies
--- require 'zebra.core.zebra'
-
--- -- load application modules
--- try_require('config.application')
--- try_require('config.routes')
--- try_require('db.db')
-
