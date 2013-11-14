1bebf38de92e4a4a5400b12a2b5a3f8ecd0902e2
Roberto Ostinelli <roberto@ostinelli.net>
2013-11-13 19:46:18 -0500

    Add SqlDatabase.

---
+++ gin/db/sql.lua
@@ -0,0 +1,42 @@
+
+local required_options = {
+    adapter = true,
+    host = true,
+    port = true,
+    database = true,
+    user = true,
+    password = true,
+    pool = true
+}
+
+local SqlDatabase = {}
+SqlDatabase.__index = SqlDatabase
+
+
+function SqlDatabase.new(options)
+    -- check for required params
+    local remaining_options = required_options
+    for k, _ in pairs(options) do remaining_options[k] = nil end
+    local missing_options = {}
+    for k, _ in pairs(remaining_options) do tinsert(missing_options, k) end
+
+    if #missing_options > 0 then error("missing required database options: " .. tconcat(missing_options, ', ')) end
+
+    -- init adapter
+    local adapter = require 'gin.db.sql.mysql.adapter'
+
+    -- init instance
+    local instance = {
+        options = options,
+        adapter = adapter
+    }
+    setmetatable(instance, SqlDatabase)
+
+    return instance
+end
+
+function SqlDatabase:execute(sql)
+    return self.adapter.execute(self.options, sql)
+end
+
+return SqlDatabase
---
+++ spec/db/sql_spec.lua
@@ -0,0 +1,79 @@
+require 'spec.spec_helper'
+
+describe("Database SQL", function()
+    before_each(function()
+        SqlDatabase =  require 'gin.db.sql'
+
+        options = {
+            adapter = 'mysql',
+            host = "127.0.0.1",
+            port = 3306,
+            database = "gin_development",
+            user = "root",
+            password = "",
+            pool = 5
+        }
+
+        package.loaded['gin.db.sql.mysql.adapter'] = {
+            name = 'adapter',
+            execute = function(...) options_arg, sql_arg = ... end
+        }
+
+        package.loaded['gin.db.model'] = {
+            new = function(...)
+                database_arg, table_name_arg = ...
+                return 'new-model'
+            end
+        }
+    end)
+
+
+    after_each(function()
+        package.loaded['gin.db.sql'] = nil
+        SqlDatabase = nil
+        options = nil
+        package.loaded['gin.db.sql.mysql.adapter'] = nil
+        options_arg = nil
+        sql_arg = nil
+        database_arg = nil
+        table_name_arg = nil
+        package.loaded['gin.db.model'] = nil
+    end)
+
+    describe(".new", function()
+        describe("when all the required options are passed", function()
+            it("initializes an instance", function()
+                local DB = SqlDatabase.new(options)
+                assert.are.equal(options, DB.options)
+                assert.are.equal('adapter', DB.adapter.name)
+            end)
+        end)
+
+        describe("when not all the required options are passed", function()
+            it("raises an error", function()
+                options = {
+                    adapter = 'mysql',
+                    host = "127.0.0.1",
+                    user = "root",
+                    password = "",
+                    pool = 5
+                }
+
+                ok, err = pcall(function() return db.new(options) end)
+                assert.are.equal(false, ok)
+                assert.are.not_equals(true, string.match(err, "missing required database options: database, port"))
+            end)
+        end)
+    end)
+
+    describe("#execute", function()
+        it("calls the execute on the adapter", function()
+            local DB = SqlDatabase.new(options)
+
+            DB:execute("SELECT 1;")
+
+            assert.are.equal(options, options_arg)
+            assert.are.equal("SELECT 1;", sql_arg)
+        end)
+    end)
+end)
