9b0dd01b01feb133d30f7cd42313cbe299882165
Roberto Ostinelli <roberto@ostinelli.net>
2013-11-19 18:48:09 -0500

    Fix adapter being hardcoded to MySQL and ensure params get redefined entirely.
    [#60908070]

--- gin/db/sql.lua
+++ gin/db/sql.lua
@@ -5,31 +5,29 @@ local require = require
 local setmetatable = setmetatable
 
 
-local required_options = {
-    adapter = true,
-    host = true,
-    port = true,
-    database = true,
-    user = true,
-    password = true,
-    pool = true
-}
-
 local SqlDatabase = {}
 SqlDatabase.__index = SqlDatabase
 
 
 function SqlDatabase.new(options)
     -- check for required params
-    local remaining_options = required_options
-    for k, _ in pairs(options) do remaining_options[k] = nil end
+    local required_options = {
+        adapter = true,
+        host = true,
+        port = true,
+        database = true,
+        user = true,
+        password = true,
+        pool = true
+    }
+    for k, _ in pairs(options) do required_options[k] = nil end
     local missing_options = {}
-    for k, _ in pairs(remaining_options) do tinsert(missing_options, k) end
+    for k, _ in pairs(required_options) do tinsert(missing_options, k) end
 
     if #missing_options > 0 then error("missing required database options: " .. tconcat(missing_options, ', ')) end
 
     -- init adapter
-    local adapter = require 'gin.db.sql.mysql.adapter'
+    local adapter = require('gin.db.sql.' .. options.adapter .. '.adapter')
 
     -- init instance
     local instance = {
