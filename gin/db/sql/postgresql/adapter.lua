fee26106689856fb6ec39b4ad37614d9ccb0636c
Roberto Ostinelli <roberto@ostinelli.net>
2013-11-23 13:46:13 -0500

    Complete PostgreSQL adapter.
    [#60019012]

--- gin/db/sql/postgresql/adapter.lua
+++ gin/db/sql/postgresql/adapter.lua
@@ -1,6 +1,9 @@
 -- perf
+local error = error
+local require = require
+local smatch = string.match
 local tconcat = table.concat
-
+local tonumber = tonumber
 
 local PostgreSql = {}
 PostgreSql.default_database = 'postgres'
@@ -34,16 +37,38 @@ end
 
 -- return list of tables
 function PostgreSql.tables(options)
+    local sql = "SELECT table_name FROM information_schema.tables WHERE table_catalog='" .. options.database .. "' AND table_schema = 'public';"
+    local res = PostgreSql.execute(options, sql)
+    local tables = {}
 
+    for _, v in pairs(res) do
+        for _, table_name in pairs(v) do
+            tinsert(tables, table_name)
+        end
+    end
+
+    return tables
 end
 
 -- return schema as a table
 function PostgreSql.schema(options)
+    local Migration = require 'gin.db.sql.migrations'
+    local schema = {}
 
+    local tables = PostgreSql.tables(options)
+    for i, table_name in ipairs(tables) do
+        if table_name ~= Migration.migrations_table_name then
+            local sql = "SELECT column_name, column_default, is_nullable, data_type, character_maximum_length, numeric_precision, datetime_precision FROM information_schema.columns WHERE table_name ='" .. table_name .. "';"
+            local columns_info = PostgreSql.execute(options, "SHOW COLUMNS IN " .. sql .. ";")
+            tinsert(schema, { [table_name] = columns_info })
+        end
+    end
+
+    return schema
 end
 
--- execute a query
-function PostgreSql.execute(options, sql)
+-- execute query on db
+local function db_execute(options, db, sql)
     local location = PostgreSql.execute_location_for(options)
 
     -- execute query
@@ -66,9 +91,22 @@ function PostgreSql.execute(options, sql)
     end
 end
 
+-- execute a query
+function PostgreSql.execute(options, sql)
+    return db_execute(options, db, sql)
+end
+
+function append_to_sql(sql, append_sql)
+    local sql_without_last_semicolon = smatch(sql, "(.*);?")
+    return sql_without_last_semicolon .. append_sql
+end
+
 -- execute a query and return the last ID
 function PostgreSql.execute_and_return_last_id(options, sql)
-
+    -- execute query and get last id
+    sql = append_to_sql(sql, " RETURNING id;")
+    local res = db_execute(options, db, sql)
+    return tonumber(res[1].id)
 end
 
 return PostgreSql
