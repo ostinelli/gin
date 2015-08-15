d1ddcedb51543cc39c034e8645c66ebdca89c757
idevz <15134559390@163.com>
2015-08-15 16:58:15 +0800

    a package.path problem
    install gin by luarocks, the gin lib default install under the luarockt
    path like /usr/local/luarocks-2.2.2/share/lua/5.1/,so run the gin
    console like os.execute("lua -i -e…”) will get a not find module mistake

--- gin/cli/console.lua
+++ gin/cli/console.lua
@@ -10,7 +10,10 @@ local GinConsole = {}
 
 function GinConsole.start()
     print(ansicolors("Loading %{cyan}" .. Gin.env .. "%{reset} environment (Gin v" .. Gin.version .. ")"))
-    os.execute("lua -i -e \"require 'gin.core.detached' require 'gin.helpers.command'\"")
+    -- os.execute("lua -i -e \"require 'gin.core.detached' require 'gin.helpers.command'\"")
+    os.execute([[
+    	lua -i -e "package.path='/usr/local/luarocks-2.2.2/share/lua/5.1/?.lua;'..package.path; package.cpath='/usr/local/luarocks-2.2.2/lib/lua/5.1/?.so;'..package.cpath; require 'gin.core.detached' require 'gin.helpers.command'"
+    	]])
 end
 
 return GinConsole
